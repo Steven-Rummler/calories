ee6fdb66c49cecd998bdc78a58489de969d31eea
Steven Rummler <steven.rummler@gmail.com>
2022-10-01 22:27:14 -0600

    Saving state to local storage

--- App.js
+++ App.js
@@ -5,19 +5,22 @@ import LogEntryScreen from './screens/logEntry';
 import HistoryScreen from './screens/history';
 import StatsScreen from './screens/stats';
 import { Provider } from 'react-redux';
-import { store } from './store.js'
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store.js'
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return <Provider store={store}>
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Log Entry" component={LogEntryScreen} />
-        <Stack.Screen name="History" component={HistoryScreen} />
-        <Stack.Screen name="Stats" component={StatsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PersistGate loading={null} persistor={persistor}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Log Entry" component={LogEntryScreen} />
+          <Stack.Screen name="History" component={HistoryScreen} />
+          <Stack.Screen name="Stats" component={StatsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PersistGate>
   </Provider>;
 }
--- store.js
+++ store.js
@@ -1,4 +1,11 @@
 import { configureStore, combineReducers, createSlice } from "@reduxjs/toolkit";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage
+}
 
 const slice = createSlice({
     name: 'data',
@@ -23,6 +30,17 @@ const reducer = combineReducers({
     data: slice.reducer
 });
 
-const store = configureStore({ reducer });
+const persistedReducer = persistReducer(persistConfig, reducer);
 
-export { store, updateData, addEntry, getEntries };
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor, updateData, addEntry, getEntries };
