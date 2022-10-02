67870612478c5e91a93090e07b0dcd75abe36feb
Steven Rummler <steven.rummler@gmail.com>
2022-10-01 19:05:14 -0600

    History first go

--- screens/history.js
+++ screens/history.js
@@ -1,15 +1,42 @@
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import { useSelector } from 'react-redux';
 import { store, getEntries } from '../store';
+import { StatusBar } from 'expo-status-bar';
 
 export default function HistoryScreen({ navigation }) {
     const entries = useSelector(getEntries);
 
-    console.log(entries);
-
-    return (
-        <View>
-            <Text>History</Text>
+    const renderItem = ({ item }) => (
+        <View style={styles.item}>
+            <Text>Date: {item.data}</Text>
+            <Text>Type: {item.entryType}</Text>
+            {item.label ? (<Text>Label: {item.label}</Text>) : null}
+            <Text>Number: {item.number}</Text>
         </View>
     );
+
+    return (
+        <View style={styles.container}>
+            <FlatList data={entries}
+                renderItem={renderItem}
+                keyExtractor={item => item.number}
+            />
+        </View>
+    );
+}
+
+const styles = {
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 32,
+    },
 }
