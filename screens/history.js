698a88cbd19e71698d9669d7fffe1d8c7d4625a5
Steven Rummler <steven.rummler@gmail.com>
2022-10-16 20:21:18 -0600

    fixed typo

--- screens/history.js
+++ screens/history.js
@@ -58,5 +58,5 @@ function DateDisplay({ date }) {
 
 const getEntriesForType = (entries, entryType) => entries.filter(entry => entry.entryType === entryType);
 const getEntriesForDate = (entries, date) => entries.filter(entry => date.isSame(entry.date, 'day'));
-const getMinDate = entries => reduce((min, next) => (!min || next.date.isBefore(min)) ? next.date : min, null) || dayjs();
-const getMaxDate = entries => reduce((max, next) => (!max || next.date.isAfter(max)) ? next.date : max, null) || dayjs();
+const getMinDate = entries => entries.reduce((min, next) => (!min || next.date.isBefore(min)) ? next.date : min, null) || dayjs();
+const getMaxDate = entries => entries.reduce((max, next) => (!max || next.date.isAfter(max)) ? next.date : max, null) || dayjs();
