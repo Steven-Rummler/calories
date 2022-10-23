2ae064266173751c499471c66aadaadacf7f5547
Steven Rummler <steven.rummler@gmail.com>
2022-10-23 17:04:39 -0600

    entries filtering and date boundaries

--- pure/entries/index.js
+++ pure/entries/index.js
@@ -2,11 +2,16 @@ const dayjs = require('dayjs');
 const { entryTypes } = require('../entryTypes');
 
 const getEntriesForType = (entries, entryType) => {
+    if (!Array.isArray(entries)) throw Error('Invalid entries');
     if (!entryTypes.includes(entryType)) throw Error('Invalid entry type');
     return entries.filter(entry => entry.entryType === entryType);
 }
-const getEntriesForDate = (entries, date) => entries.filter(entry => date.isSame(entry.date, 'day'));
-const getMinDate = entries => entries.reduce((min, next) => (!min || next.date.isBefore(min)) ? next.date : min, null) || dayjs();
-const getMaxDate = entries => entries.reduce((max, next) => (!max || next.date.isAfter(max)) ? next.date : max, null) || dayjs();
+const getEntriesForDate = (entries, date) => {
+    if (!Array.isArray(entries)) throw Error('Invalid entries');
+    if (!dayjs.isDayjs(date)) throw Error('Invalid date');
+    return entries.filter(entry => date.isSame(entry.date, 'day'));
+}
+const getMinDate = entries => entries.reduce((min, next) => (!min || next.date.isBefore(min)) ? next.date : min, null);
+const getMaxDate = entries => entries.reduce((max, next) => (!max || next.date.isAfter(max)) ? next.date : max, null);
 
-module.exports = { getEntriesForType };
+module.exports = { getEntriesForType, getEntriesForDate, getMinDate, getMaxDate };
--- pure/entries/test.js
+++ pure/entries/test.js
@@ -1,11 +1,70 @@
-const { getEntriesForType } = require('.');
+const { getEntriesForType, getEntriesForDate, getMinDate, getMaxDate } = require('.');
+const dayjs = require('dayjs');
 
 describe('getEntriesForType', () => {
     test('should validate entries', () => {
-        expect(() => getEntriesForType(null, null)).toThrow();
+        expect(() => getEntriesForType(null, null)).toThrow('Invalid entries');
     })
 
     test('should validate entryType', () => {
-        expect(() => getEntriesForType([], null)).toThrow();
+        expect(() => getEntriesForType([], null)).toThrow('Invalid entry type');
+    })
+
+    test('should filter by entryType', () => {
+        const entries = [{
+            entryType: 'food'
+        }, {
+            entryType: 'active'
+        }]
+        expect(getEntriesForType(entries, 'food').length).toBe(1);
+    })
+})
+
+describe('getEntriesForDate', () => {
+    test('should validate entries', () => {
+        expect(() => getEntriesForDate(null, null)).toThrow('Invalid entries');
+    })
+
+    test('should validate date', () => {
+        expect(() => getEntriesForDate([], null)).toThrow('Invalid date');
+    })
+
+    test('should filter by date', () => {
+        const entries = [{
+            date: dayjs('2022-01-01')
+        }, {
+            date: dayjs('2022-01-02')
+        }]
+        expect(getEntriesForDate(entries, dayjs('2022-01-01')).length).toBe(1);
+    })
+})
+
+describe('getMinDate', () => {
+    test('should return null for empty array', () => {
+        expect(getMinDate([])).toBe(null);
+    })
+
+    test('should return correct date', () => {
+        const entries = [{
+            date: dayjs('2022-01-01')
+        }, {
+            date: dayjs('2022-01-02')
+        }]
+        expect(getMinDate(entries).isSame(dayjs('2022-01-01'), 'day')).toBeTruthy();
+    })
+})
+
+describe('getMaxDate', () => {
+    test('should return null for empty array', () => {
+        expect(getMaxDate([])).toBe(null);
+    })
+
+    test('should return correct date', () => {
+        const entries = [{
+            date: dayjs('2022-01-01')
+        }, {
+            date: dayjs('2022-01-02')
+        }]
+        expect(getMaxDate(entries).isSame(dayjs('2022-01-02'), 'day')).toBeTruthy();
     })
 })
